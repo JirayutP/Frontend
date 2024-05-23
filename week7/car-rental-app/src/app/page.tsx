afa5c0dc72b75e28246feaf9619589ccfec2ff05
JirayutP <6432022721@student.chula.ac.th>
2024-05-23 18:49:23 +0700

    week7

---
+++ week7/car-rental-app/src/app/page.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import Banner from '@/components/Banner'
+import styles from './page.module.css'
+import ProductCard from '@/components/ProductCard'
+import CarPanel from '@/components/CarPanel'
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        {/* <h1 style={{color: "rgb(0,0,0)"}}>Hello World</h1> */}
+        <Banner/>
+      </div>
+    </main>
+  )
+}
